cb8320089c6f07fd37824a67086a8170ff96bab1
bon300-6 <1234suho@naver.com>
2022-03-18 14:53:57 +0900

    2022/03/16

---
+++ src/frontend/src/data/index.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        writer: '예진',
+        title: 'WDa',
+        content: '1'
+    },
+    {
+        writer: '예진',
+        title: 'Wda2',
+        content: '2'
+    },
+    {
+        writer: '예진',
+        title: 'Wda3',
+        content: '3'
+    }
+]
