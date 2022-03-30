2a55246a70842ecf1ffe391708d4f2ef5def60cf
yeahjinnnn <leesun02130@naver.com>
2022-03-30 21:01:53 +0900

    2022/03/30 개고생했음..

---
+++ src/vue-board/src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { router } from './router'
+import axios from "axios";
+
+axios.defaults.headers.common['Access-Control_Allow-Origin'] = '*'
+axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8'
+
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+app.config.globalProperties.axios = axios
---
+++ src/vue-board/src/router/index.js
@@ -0,0 +1,59 @@
+
+import { createWebHistory, createRouter } from 'vue-router'
+import List from '@/components/board/List'             //게시판 리스트 컴포넌트 호출
+import HelloWorld from "@/components/HelloWorld";   //메인 컴포넌트 호출
+import Write from '@/components/board/Write';  //게시판 리스트 컴포넌트 호출
+import Detail from '@/components/Detail';
+import Create from '@/components/Create';
+import Read from "@/components/Read";
+
+
+
+const routes = [
+  {
+    path: '/',
+    name: 'HelloWorld',
+    component: HelloWorld
+  },
+  {
+    path:'/board/list'
+    ,name:List
+    ,component:List
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+  },
+  {
+    path:'/board/write',
+    name:Write,
+    component: Write
+  },
+  {
+    path: '/detail/:contentId',
+    name: 'Detail',
+    component: Detail
+  },
+  {
+    path: '/read',
+    name: Read,
+    component: Read
+  },
+  {
+    path: '/create/:contentId?',
+    name: 'Create',
+    component: Create
+  },
+  {
+    path: '/create',
+    name: 'Create',
+    component: Create
+  }
+
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+
