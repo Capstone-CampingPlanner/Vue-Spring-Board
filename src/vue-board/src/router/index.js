e840d952e32a5e956cc33bb15028d556f35e3286
yeahjinnnn <leesun02130@naver.com>
2022-04-07 11:35:36 +0900

    2022/04/06 게시글 CRUD 구현성공

--- src/vue-board/src/router/index.js
+++ src/vue-board/src/router/index.js
@@ -1,6 +1,5 @@
 
 import { createWebHistory, createRouter } from 'vue-router'
-import List from '@/components/board/List'             //게시판 리스트 컴포넌트 호출
 import HelloWorld from "@/components/HelloWorld";   //메인 컴포넌트 호출
 import Create from '@/components/Create';
 import Read from "@/components/Read";
@@ -15,14 +14,7 @@ const routes = [
     name: 'HelloWorld',
     component: HelloWorld
   },
-  {
-    path:'/board/list'
-    ,name:List
-    ,component:List
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-  },
+
   {
     path: '/read',
     name: Read,
